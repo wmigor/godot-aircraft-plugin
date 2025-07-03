b07c85e548709a0e6f6ecf24fa51f108ea464ce3
igor <wmigor@yandex.ru>
2025-07-03 03:02:40 +0300

    vlm

--- addons/aircraft/aircraft_plugin.gd
+++ addons/aircraft/aircraft_plugin.gd
@@ -2,14 +2,19 @@
 extends EditorPlugin
 
 var VehicleWing3DGizmoPlugin := preload("uid://bn51yyq6ui0ym")
+var VlmWing3DGizmoPlugin := preload("uid://dq1q8iqyl8rlu")
 
 var _vehicle_wing_gizmo: EditorNode3DGizmoPlugin
+var _vlm_wing_gizmo: EditorNode3DGizmoPlugin
 
 
 func _enter_tree() -> void:
 	_vehicle_wing_gizmo = VehicleWing3DGizmoPlugin.new()
+	_vlm_wing_gizmo = VlmWing3DGizmoPlugin.new()
 	add_node_3d_gizmo_plugin(_vehicle_wing_gizmo)
+	add_node_3d_gizmo_plugin(_vlm_wing_gizmo)
 
 
 func _exit_tree() -> void:
 	remove_node_3d_gizmo_plugin(_vehicle_wing_gizmo)
+	remove_node_3d_gizmo_plugin(_vlm_wing_gizmo)
---
+++ addons/aircraft/gizmo/vlm_wing_3d_gizmo_plugin.gd
@@ -0,0 +1,107 @@
+extends EditorNode3DGizmoPlugin
+
+
+func _init() -> void:
+	create_material("wing_material", Color.GREEN)
+	create_material("error_material", Color.RED)
+	create_material("mac_material", Color.YELLOW)
+
+
+func _get_gizmo_name() -> String:
+	return "VlmWing3D"
+
+
+func _has_gizmo(for_node_3d: Node3D) -> bool:
+	return for_node_3d is VlmWing3D
+
+
+func _redraw(gizmo: EditorNode3DGizmo) -> void:
+	gizmo.clear()
+	var wing := gizmo.get_node_3d() as VlmWing3D
+	if wing == null:
+		return
+
+	var lines := PackedVector3Array()
+	_add_panels(wing, false, lines)
+	var valid := _add_wing_lines(wing, false, lines);
+	if wing.mirror:
+		_add_wing_lines(wing, true, lines);
+
+	var material := "wing_material" if valid else "error_material"
+	gizmo.add_lines(lines, get_material(material, gizmo), false, Color.WHITE)
+
+	lines.clear()
+	_add_mac(wing, lines)
+	gizmo.add_lines(lines, get_material("mac_material", gizmo), false, Color.WHITE)
+
+
+func _add_panels(wing: VlmWing3D, mirror: bool, lines: PackedVector3Array) -> void:
+	wing._try_rebuild()
+	for panel in wing._panels:
+		for i in len(panel.points):
+			lines.append(panel.points[i])
+			lines.append(panel.points[(i + 1) % len(panel.points)])
+
+
+func _add_wing_lines(wing: VlmWing3D, mirror: bool, lines: PackedVector3Array) -> bool:
+	var base := wing.get_base()
+	var tip := wing.get_tip()
+	var chord := wing.chord
+	var taper := wing.taper
+	var twist := wing.twist
+	var mac := wing.get_mac()
+	var mac_z := wing.get_mac_forward_position()
+
+	var valid := wing.span > 0.0 and chord > 0.0 and taper >= 0.0 and wing.offset >= 0.0 and wing.get_console_length() > 0.0
+
+	base.z += mac * 0.25 - mac_z
+	tip += (mac * 0.25 - mac_z) * Vector3.BACK.rotated(Vector3.RIGHT, twist)
+
+	if mirror:
+		base.x *= -1.0
+		tip.x *= -1.0
+
+	var w1 := _get_wing_point(base, tip, 0.0, -chord * 0.5, 0.0)
+	var w2 := _get_wing_point(base, tip, 1.0, -chord * 0.5 * taper, twist)
+
+	lines.append(w1)
+	lines.append(w2)
+
+	var last_end := 0.0
+	var sections := [[0.0, 1.0]]
+	for i in len(sections):
+		var section = sections[i]
+		var start = section[0]
+		var end = section[1] 
+		var start_chord := lerpf(chord, chord * taper, start)
+		var end_chord := lerp(chord, chord * taper, end)
+		var d1 := _get_wing_point(base, tip, start, start_chord * 0.5, twist * start)
+		var d2 := _get_wing_point(base, tip, end, end_chord * 0.5, twist * end)
+		lines.append(d1)
+		lines.append(d2)
+		if i == 0:
+			lines.append(w1)
+			lines.append(d1)
+		if i == sections.size() - 1:
+			lines.append(w2)
+			lines.append(d2)
+		valid = valid and start <= end and start >= 0.0 and start <= 1.0 and end <= 1.0 and end >= 0.0 and start >= last_end
+		last_end = end
+	return valid
+
+
+func _add_mac(wing: VlmWing3D, lines: PackedVector3Array) -> void:
+	var mac := wing.get_mac()
+	var half_mac := mac * 0.5
+	var mac_z := 0.25 * mac
+	lines.append(Vector3(0.1, 0.0, mac_z + half_mac))
+	lines.append(Vector3(-0.1, 0.0, mac_z + half_mac))
+	lines.append(Vector3(0.1, 0.0, mac_z - half_mac))
+	lines.append(Vector3(-0.1, 0.0, mac_z - half_mac))
+
+
+func _get_wing_point(base: Vector3, tip: Vector3, pos: float, chord: float, twist: float) -> Vector3:
+	var point := base + (tip - base) * pos
+	var twist_dir := Vector3.BACK.rotated(Vector3.RIGHT, twist)
+	point += twist_dir * chord
+	return point
