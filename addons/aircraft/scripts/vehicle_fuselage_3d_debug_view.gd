dfb0358922f70007068720b181b24aa35b0de86c
igor <wmigor@yandex.ru>
2025-08-20 02:11:00 +0300

    vehicle fuselage 3d

---
+++ addons/aircraft/scripts/vehicle_fuselage_3d_debug_view.gd
@@ -0,0 +1,40 @@
+extends Node3D
+
+@onready var _fuselage := get_parent() as VehicleFuselage3D
+
+static var fuselage_material: Material = null
+
+
+func _ready() -> void:
+	if fuselage_material == null:
+		fuselage_material = _create_material(Color.ORANGE)
+	build()
+
+
+func build() -> void:
+	_clear()
+	if _fuselage == null:
+		return
+	for i in _fuselage.get_section_count():
+		var view := CSGBox3D.new()
+		var width := _fuselage.get_section_width(i)
+		var length := _fuselage.get_section_length(i)
+		var section_transform := _fuselage.get_section_transform(i)
+		view.material = fuselage_material
+		view.basis = section_transform.basis
+		view.position = section_transform.origin
+		view.size.x = width
+		view.size.y = width * 0.05
+		view.size.z = length * 0.95
+		add_child(view)
+
+
+func _clear() -> void:
+	for i in get_child_count():
+		get_child(i).queue_free()
+
+
+func _create_material(color: Color) -> Material:
+	var material := StandardMaterial3D.new()
+	material.albedo_color = color
+	return material
--- demo/aircraft/aircraft.gd
+++ demo/aircraft/aircraft.gd
@@ -11,6 +11,7 @@ class_name Aircraft
 @onready var wing := $Wing as VehicleWing3D
 @onready var elevator := $Elevator as VehicleWing3D
 @onready var rudder := $Rudder as VehicleWing3D
+@onready var fuselage := $Fuselage as VehicleFuselage3D
 @onready var motor := $Motor as Motor
 
 var flap_mode := 0
@@ -19,3 +20,12 @@ var flap_mode := 0
 func _ready() -> void:
 	for w in find_children("*", "VehicleWing3D"):
 		w.debug = debug
+	for f in find_children("*", "VehicleFuselage3D"):
+		f.debug = debug
+
+
+func _physics_process(_delta: float) -> void:
+	var force := wing.get_force() + elevator.get_force() + rudder.get_force()
+	var wing_drag := force.dot(-linear_velocity.normalized())
+	var fuselage_drag = fuselage._force.dot(-linear_velocity.normalized())
+	print("wing: ", roundi(wing_drag), ' fuselage: ', roundi(fuselage_drag))
