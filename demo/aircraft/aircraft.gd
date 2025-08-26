87c9c7cd5ca3c76b061952daa72abf976b6be451
igor.akulov <igor.akulov@my.games>
2025-08-26 10:14:07 +0300

    propeller

--- demo/aircraft/aircraft.gd
+++ demo/aircraft/aircraft.gd
@@ -23,9 +23,9 @@ func _ready() -> void:
 	for f in find_children("*", "VehicleFuselage3D"):
 		f.debug = debug
 
-
-func _physics_process(_delta: float) -> void:
-	var force := wing.get_force() + elevator.get_force() + rudder.get_force()
-	var wing_drag := force.dot(-linear_velocity.normalized())
-	var fuselage_drag = fuselage._force.dot(-linear_velocity.normalized())
-	print("wing: ", roundi(wing_drag), ' fuselage: ', roundi(fuselage_drag))
+#
+#func _physics_process(_delta: float) -> void:
+	#var force := wing.get_force() + elevator.get_force() + rudder.get_force()
+	#var wing_drag := force.dot(-linear_velocity.normalized())
+	#var fuselage_drag = fuselage._force.dot(-linear_velocity.normalized())
+	#print("wing: ", roundi(wing_drag), ' fuselage: ', roundi(fuselage_drag))
--- demo/aircraft/motor.gd
+++ demo/aircraft/motor.gd
@@ -1,34 +1,61 @@
 extends Node3D
 class_name Motor
 
-@export var thrust_max := 200.0
-@export var max_velocity_kmph := 80.0
-@export var rpm_max := 512.0
+@export var max_rpm := 2900.0
+@export var max_engine_power_hp := 360.0
+@export var diameter := 2.4
+@export var inertia := 10.0
+@export var density := 1.2255
 
 @onready var aircraft := get_parent() as Aircraft
 
 const TO_KMPH = 3.6
 const TO_RPM := 60.0 / TAU
+const HP_TO_W := 745.7
 
 var throttle := 1.0
 var thrust := 0.0
 var rpm := 0.0
+var rps: float:
+	get(): return rpm / 60.0
 
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var forward := -aircraft.basis.z
 	var velocity := aircraft.linear_velocity.dot(forward)
-	var max_velocity := max_velocity_kmph / TO_KMPH
-	if velocity < max_velocity:
-		thrust = thrust_max
-	else:
-		var zero_thrust_velocity := max_velocity * 1.2
-		thrust = max(0.0, lerpf(thrust_max, 0.0, (velocity - max_velocity) / (zero_thrust_velocity - max_velocity)))
-	thrust *= throttle
+	var max_engine_power := max_engine_power_hp * HP_TO_W
+	var engine_power := _get_engine_power()
+	var j := velocity / (diameter * rps) if rps > 0.01 else velocity / diameter
+	var required_power := _get_required_power(j)
+	if throttle <= 0 and rpm < 500:
+		required_power += max_engine_power * 0.1
+	var angular_velocity := rpm / TO_RPM
+	var torque := (engine_power - required_power) / angular_velocity if absf(angular_velocity) > 0.01 else (engine_power - required_power)
+	rpm += torque / inertia * delta * TO_RPM
+	if rpm < 0.0:
+		rpm = 0.0
+	thrust = _get_thrust(j)
 	aircraft.apply_force(thrust * forward, global_position - aircraft.global_position)
+	rotation.z += angular_velocity * delta
+	print(int(rpm), '   ', thrust, '   ', torque)
 
 
-func _process(delta: float) -> void:
-	var target_rpm := rpm_max * (1.0 - (1.0 - throttle) * (1.0 - throttle))
-	rpm = move_toward(rpm, target_rpm, delta * rpm_max / 3.0)
-	rotation.z += rpm / TO_RPM * delta
+func _get_engine_power() -> float:
+	var max_engine_power := max_engine_power_hp * HP_TO_W
+	if rpm > max_rpm:
+		return throttle * lerpf(max_engine_power, 0.0, (rpm - max_rpm) / (max_rpm * 0.2))
+	var min_engine_power := max_engine_power * 0.01
+	var engine_power := throttle * minf(max_engine_power, maxf(min_engine_power, lerpf(min_engine_power, max_engine_power, rpm / max_rpm)))
+	return throttle * engine_power
+
+
+func _get_required_power(j: float) -> float:
+	var x := j / 1.2
+	var cp := 0.07 * (1.0 - x)
+	return cp * density * pow(diameter, 5) * pow(rps, 3)
+
+
+func _get_thrust(j: float) -> float:
+	var x := j / 1.05
+	var ct := 0.1 * (1.0 - x * x)
+	return ct * density * pow(diameter, 4) * pow(rps, 2)
