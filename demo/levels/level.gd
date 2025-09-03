0aca4ec6d3342fac227e939f95211804fab73ce5
igor <wmigor@yandex.ru>
2025-09-04 00:34:04 +0300

    toggle fullscreen (alt + enter)

--- demo/levels/level.gd
+++ demo/levels/level.gd
@@ -48,8 +48,17 @@ func clear() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("spawn_next"):
 		spawn_next()
+	elif event.is_action_pressed("toggle_fullscreen"):
+		toggle_fullscreen()
 
 
 func spawn_next() -> void:
 	if len(aircrafts) > 0:
 		spawn_aircraft((aircraft_index + 1) % len(aircrafts))
+
+
+func toggle_fullscreen() -> void:
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
