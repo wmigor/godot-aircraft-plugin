2e7338b2e54b0c3c014f0623e33ea8f6d6d41940
igor <wmigor@yandex.ru>
2026-05-13 00:19:57 +0300

    propeller plot

--- addons/aircraft/scripts/vehicle_propeller_3d.gd
+++ addons/aircraft/scripts/vehicle_propeller_3d.gd
@@ -4,11 +4,9 @@ class_name VehiclePropeller3D
 
 @export var airfoil: Airfoil
 @export var blade_count := 2.0
-@export var root_chord := 0.102
-@export var tip_chord := 0.076
-@export var section_count := 10
-@export var hub_radius := 0.15
-@export_range(-75, 75, 0.001, "radians_as_degrees") var root_pitch := deg_to_rad(40.0)
+@export var section_count := 20
+@export var hub_radius := 0.1
+@export_range(-75, 75, 0.001, "radians_as_degrees") var root_pitch := deg_to_rad(35.1)
 @export_range(-75, 75, 0.001, "radians_as_degrees") var tip_pitch := deg_to_rad(12.7)
 @export var sound_speed := 340.3
 
@@ -27,17 +25,32 @@ var _sections: Array[Section]
 func _ready() -> void:
 	var blade_length := radius - hub_radius
 	var section_length := blade_length / section_count
-	var blade_area := (root_chord + tip_chord) * 0.5 * blade_length
-	var aspect_ratio := (blade_length * blade_length / blade_area) if blade_area > 0 else 1.0
+	var total_chord_sum := 0.0
 	for section_index in section_count:
 		var section := Section.new()
 		section.radius = hub_radius + section_index * section_length + section_length * 0.5
-		var fraction := (section.radius - hub_radius) / blade_length
-		section.chord = lerpf(root_chord, tip_chord, fraction)
+		section.chord = _get_mc_cauley_1c160_chord(section.radius)
 		section.area = section_length * section.chord
-		section.pitch = lerpf(root_pitch, tip_pitch, fraction)
-		section.aspect_ratio = aspect_ratio
+		section.pitch = _get_mc_cauley_1c160_pitch(section.radius)
+		total_chord_sum += section.chord
 		_sections.append(section)
+	var avg_chord := total_chord_sum / section_count
+	var blade_area := avg_chord * blade_length
+	var aspect_ratio := (blade_length * blade_length / blade_area) if blade_area > 0 else 1.0
+	for section in _sections:
+		section.aspect_ratio = aspect_ratio
+
+
+func _get_mc_cauley_1c160_chord(section_radius: float) -> float:
+	var x := section_radius / radius
+	var base_chord = 0.1286 * pow(x, 3) - 0.5597 * pow(x, 2) + 0.5061 * x - 0.0029
+	var scale_factor := radius / 0.9525
+	return maxf(0.01, base_chord * scale_factor)
+
+
+func _get_mc_cauley_1c160_pitch(section_radius: float) -> float:
+	var geometric_pitch_dist := tan(tip_pitch) * 2.0 * PI * radius
+	return atan2(geometric_pitch_dist, 2.0 * PI * section_radius)
 
 
 func _calculate_factors(wind_velocity: float) -> void:
--- demo/plots/propeller_stand.gd
+++ demo/plots/propeller_stand.gd
@@ -26,26 +26,22 @@ func _ready() -> void:
 	_build_plots()
 
 
-func _process(_delta: float) -> void:
-	_build_plots()
-
-
 func _build_plots() -> void:
 	_plots_view.clear()
-	_generate_plot()
-	#for propeller in find_children("*", "VehiclePropeller3D"):
-		#_build_plot(propeller)
+	#_generate_plot()
+	for propeller in find_children("*", "VehiclePropeller3D"):
+		_build_plot(propeller)
 
 
-func _build_plot(propeller: VehiclePropeller3D, velocity_min := 0, velocity_max := 200, step := 10) -> void:
+func _build_plot(propeller: VehiclePropeller3D, velocity_min := 10, velocity_max := 300, step := 1) -> void:
 	var thrusts := PackedVector2Array()
 	var powers := PackedVector2Array()
 	for velocity in range(velocity_min, velocity_max, step):
-		propeller.angular_velocity = 500 / VehiclePropeller3D.TO_RPM
-		propeller._calculate_factors(velocity / VehiclePropeller3D.TO_KMPH)
-		#var d := propeller.radius * 2.0
-		#var j := velocity / (d * propeller.rps) if absf(propeller.rps) > 0.001 else velocity / diameter
-		var j := velocity / propeller.angular_velocity
+		propeller.angular_velocity = 2500 / VehiclePropeller3D.TO_RPM
+		for i in range(10):
+			propeller._calculate_factors(velocity / VehiclePropeller3D.TO_KMPH)
+		var d := propeller.radius * 2.0
+		var j := velocity / (d * propeller.rps) if absf(propeller.rps) > 0.001 else velocity / diameter
 		thrusts.append(Vector2(j, propeller._thrust_factor))
 		powers.append(Vector2(j, propeller._power_required_factor))
 	_plots_view.add_plot(PlotsView.Data.new(thrusts, Color.BLUE, 2.0))
